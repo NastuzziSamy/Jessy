c06152b593d2353f86d857e108cb5c8048ee0bff
NastuzziSamy <samy@nastuzzi.fr>
2017-12-13 18:20:08 +0100

    Add reservation API
    Can now manage shotgun reservations

--- app/src/main/java/fr/utc/simde/jessy/responses/ReservationResponse.java
+++ app/src/main/java/fr/utc/simde/jessy/responses/ReservationResponse.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  * Created by Samy on 10/11/2017.
  */
 
-public class ComedmusResponse extends APIResponse {
+public class ReservationResponse extends APIResponse {
     protected Integer reservation_id;
     protected String seance;
 
